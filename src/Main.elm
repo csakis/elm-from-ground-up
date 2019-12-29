1c84ae85a181d4769fbfa3da2a9dad5353bcfebf
Csaba Kiss <csakis@gmail.com>
2019-12-29 08:39:50 -0700

    updated to 0.19.1

--- src/Main.elm
+++ src/Main.elm
@@ -1,37 +1,57 @@
+module Main exposing (Model, Msg(..), init, main, update, view)
+
 import Browser
-import Html exposing ( Html, text, p )
-import Html.Events exposing ( onClick )
+import Html exposing (Html, div, p, text)
+import Html.Events exposing (onClick)
+
 
 main =
-  Browser.sandbox
-    { init = init
-    , update = update
-    , view = view
-    }
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
 
 
 init : Model
-init = 
-  "Hello World"
+init =
+    "hello world"
+
 
 
 -- MODEL
+
+
 type alias Model =
-  String
+    String
 
 
-type Msg = 
-  Capitalize
+type Msg
+    = Capitalize
+    | Decapitalize
+
+
 
 -- UPDATE
+
+
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Capitalize ->
-      String.toUpper model
+    case msg of
+        Capitalize ->
+            String.toUpper model
+
+        Decapitalize ->
+            String.toLower model
+
 
 
 -- VIEW
+
+
 view : Model -> Html Msg
 view model =
-  p [ onClick Capitalize ] [ text model ]
+    div []
+        [ p [ onClick Capitalize ] [ text model ]
+        , p [ onClick Decapitalize ] [ text "make it small" ]
+        ]
