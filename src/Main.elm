587cfd47ccd8fd82c78517f3a15879d45c112a36
Csaba Kiss <csakis@gmail.com>
2019-12-28 18:02:35 -0700

    added more complex state and buttons

--- src/Main.elm
+++ src/Main.elm
@@ -1,34 +1,63 @@
+module Main exposing (Model, Msg, init, main, update, view)
+
 import Browser
-import Html exposing (Html)
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+import String exposing (fromInt)
+
 
 main =
-  Browser.sandbox
-    { init = init
-    , update = update
-    , view = view
-    }
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
 
 
 init : Model
-init = 
-  "Hello World"
+init =
+    { message = "Hello World"
+    , counter = 0
+    }
+
 
 
 -- MODEL
+
+
 type alias Model =
-  String
+    { message : String
+    , counter : Int
+    }
 
 
-type alias Msg = 
-  String
+type Msg
+    = Increment
+    | Decrement
+
+
 
 -- UPDATE
+
+
 update : Msg -> Model -> Model
 update msg model =
-  model
+    case msg of
+        Increment ->
+            { model | counter = model.counter + 1 }
+        Decrement ->
+            { model | counter = model.counter - 1 }
+
 
 
 -- VIEW
-view : Model -> Html.Html Model
+
+
+view : Model -> Html Msg
 view model =
-  Html.text "Hello World"
+    div []
+        [ text model.message
+        , div [] [ text (String.fromInt model.counter) ]
+        , button [ onClick Increment ] [ text "+" ]
+        , button [ onClick Decrement ] [ text "-" ]
+        ]
